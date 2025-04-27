7557da16cbdec98f97382f6c5f6d47ffa9f4c4a5
ririsahu <ririsahu1600@gmail.com>
2025-04-27 10:01:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var container = document.querySelector("#container");
+var love = document.querySelector("i");
+
+container.addEventListener("click", function() {
+    
+    love.style.opacity = 1;
+    love.style.transform.scale = 1
+    setTimeout(function(){
+        love.style.opacity = 0;
+        love.style.transform.scale = 0
+    },1000)
+});
